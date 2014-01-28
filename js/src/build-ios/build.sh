f4e1b6b2ab67d4e2ae8e4501da37a2ef36e3fbf3
James Chen <jianhua.chen@cocos2d-x.org>
2014-01-28 09:42:19 +0800

    Cherry Pick: Spidermonkey should be built in RELEASE mode for all platforms.
    REASON:
    We used fat library for iOS, it contains i386, armv7, armv7s architecture. But we use the same header files.
    The head file of Spidermonkey contains DEBUG macro in some methods or some classes. It may lead to weird crash.
    
    HOW TO FIX:
    Undefine all DEBUG marcos in Project Settings.
    Build all libraries in RELEASE mode.
    If you wish to debug Spidermonkey, you have to know how to build debug version of SpiderMonkey and how to set DEBUG macro in Project Setting!
    
    BUT DON'T DEFINE DEBUG macro IN OUR PROJECTS
    
    Cocos2d-x is using COCOS2D_DEBUG for debugging.

--- js/src/build-ios/build.sh
+++ js/src/build-ios/build.sh
@@ -46,6 +46,7 @@ CPU_ARCH=armv7
 ARCH_OPTION="--with-ios-arch=${CPU_ARCH}"
 YARR_JIT_OPTION="--disable-yarr-jit"
 THUMB_OPTION="--with-thumb=yes"
+DEBUG_OPTION="--disable-debug"
 build_with_arch
 
 
@@ -56,6 +57,16 @@ CPU_ARCH=armv7s
 ARCH_OPTION="--with-ios-arch=${CPU_ARCH}"
 YARR_JIT_OPTION="--disable-yarr-jit"
 THUMB_OPTION="--with-thumb=yes"
+DEBUG_OPTION="--disable-debug"
+build_with_arch
+
+# create arm64 version
+IOS_TARGET=iPhoneOS
+CPU_ARCH=arm64
+ARCH_OPTION="--with-ios-arch=${CPU_ARCH}"
+YARR_JIT_OPTION="--disable-yarr-jit"
+THUMB_OPTION="--with-thumb=yes"
+DEBUG_OPTION="--disable-debug"
 build_with_arch
 
 # remove everything but the static libraries and this script
@@ -65,14 +76,6 @@ ls | grep -v build.sh | grep -v $RELEASE_DIR | xargs rm -rf
 IOS_TARGET=iPhoneSimulator
 CPU_ARCH=i386
 DEBUG_OPTION="--enable-debug"
-build_with_arch
-
-# create arm64 version
-IOS_TARGET=iPhoneOS
-CPU_ARCH=arm64
-ARCH_OPTION="--with-ios-arch=${CPU_ARCH}"
-YARR_JIT_OPTION="--disable-yarr-jit"
-THUMB_OPTION="--with-thumb=yes"
 build_with_arch
 
 #
