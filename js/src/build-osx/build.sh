ebaa7b27244766bfc13f7c568ee509651e85698d
Michael Boccara <michael@slatescience.com>
2014-02-18 18:56:31 +0200

    still build OSX in debug (it is a simulator)

--- js/src/build-osx/build.sh
+++ js/src/build-osx/build.sh
@@ -7,7 +7,7 @@ cpus=$(sysctl hw.ncpu | awk '{print $2}')
             --enable-strip --enable-strip-install \
             --disable-root-analysis --disable-exact-rooting --enable-gcincremental --enable-optimize=-O3 \
             --enable-llvm-hacks \
-            --disable-debug \
+            --enable-debug \
             --enable-intl-api=no
 # make
 make -j$cpus
